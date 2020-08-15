988ec619f917fb62d9c19ff6cf75ec3360b82a19
N Tessa Pierce <ntpierce@gmail.com>
2020-08-15 09:09:14 -0700

    fix docs

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ CLASSIFIERS = [
     "Natural Language :: English",
     "Operating System :: POSIX :: Linux",
     "Operating System :: MacOS :: MacOS X",
-    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.7",
     "Topic :: Scientific/Engineering :: Bio-Informatics",
 ]
 
