1fad96ef6b42ae1a27b167e43fd2fe38c85544ad
N Tessa Pierce <ntpierce@gmail.com>
2021-06-24 16:24:33 -0700

    simplify utils

--- thumper/common.snakefile
+++ thumper/common.snakefile
@@ -3,12 +3,12 @@
 # check config files only
 rule check:
     input:
-        config["sample_list"]                  # do nothing - this file should exist
+        config["sample_info"]                  # do nothing - this file should exist
 
 # print out the configuration
 rule showconf:
     input:
-        config["sample_list"]
+        config["sample_info"]
     run:
         import yaml
         print('# full aggregated configuration:')
--- thumper/utils.py
+++ thumper/utils.py
@@ -4,6 +4,18 @@ import pandas as pd
 from snakemake.io import expand
 from snakemake.workflow import srcdir
 
+__all__ = ['find_input_file', 'read_samples',
+           'check_and_set_alpha_ksize',
+           'load_databases_csv', 'find_valid_databases',
+           'check_and_set_databases', 'process_user_config']
+
+
+def sanitize_path(path):
+    # expand `~`, get absolute path
+    path = os.path.expanduser(path)
+    path = os.path.abspath(path)
+    return path
+
 
 def find_input_file(filename, name="input", add_paths=[], add_suffixes = ['.yaml', '.yml'], verbose = False):
     # for any file specified via command line, check if it exists at the current path, if not, try some other paths before returning  a helpful error
@@ -21,14 +33,27 @@ def find_input_file(filename, name="input", add_paths=[], add_suffixes = ['.yaml
                 if os.path.exists(tryfile) and not os.path.isdir(tryfile):
                     found_file = os.path.realpath(tryfile)
                     break
-    assert found_file, f'Error, cannot find specified {name} file {filename}\n\n\n'
+    if not found_file:
+        raise ValueError(f'Cannot find specified {name} file {filename}')
     if verbose:
         sys.stderr.write(f'\tFound {name} at {found_file}\n')
     return found_file
 
 
-def read_samples(samples_file, data_dir, strict_mode=False):
-    samples_file = find_input_file(samples_file)
+def _file_exists(filename, data_dir):
+    fname = os.path.join(data_dir, filename)
+    return os.path.isfile(fname)
+
+
+def read_samples(config, *, strict=False):
+    try:
+        filename = config.get('sample_info', '')
+        samples_file = find_input_file(filename)
+    except ValueError as exc:
+        print('** ERROR: Please provide proteomes/genomes as a txt file ' \
+        '(one filename per line) or a csv file("sample,filename"; no headers ' \
+        'using "sample_list:" in the config')
+        sys.exit(-1)
     if '.tsv' in samples_file or '.csv' in samples_file:
         separator = '\t'
         if '.csv' in samples_file:
@@ -50,207 +75,154 @@ def read_samples(samples_file, data_dir, strict_mode=False):
         samplesD = {"sample":sample_list,"filename":sample_list} # maybe later try removing *fa.gz or the like
         samples = pd.DataFrame(samplesD)
 
+    data_dir = config.get('data_dir', '')
+    if data_dir:
+        data_dir = sanitize_path(data_dir)
+    # check if file exists
+    samples['exists'] = samples['filename'].apply(_file_exists, data_dir=data_dir)
+    #import pdb;pdb.set_trace()
+    if not pd.Series(samples['exists']).all():
+        missing_files = samples[samples['exists'] == False].index
+        if strict:
+            raise ValueError(f'Sample files {",".join(missing_files)} do not exist in {data_dir}')
+        else:
+            print(f'Sample files {",".join(missing_files)} do not exist in {data_dir}')
+            print('Strict mode is OFF. Attempting to continue.')
+            samples = samples[samples['exists'] == True]
     samples.set_index("sample", inplace=True)
-
-    # Now, verify that all genome files exist
-    data_dir = sanitize_path(data_dir)
-    sample_list = samples["filename"].tolist()
-    for filename in sample_list:
-        fullpath = os.path.join(data_dir, filename)
-        if not os.path.exists(fullpath):
-            print(f'** ERROR: genome file {filename} does not exist in {data_dir}')
-            if strict_mode:
-                print('** exiting.')
-                sys.exit(-1)
-
-    return samples
+    config['samples'] = samples
+    return config
 
 
-def check_input_type(config, strict_mode=False):
+def check_and_set_alpha_ksize(config, *, strict=False):
     """
-    Check the input type provided in the configfile
+    Check the input type, alphabet, and ksize specified in the configfile.
+    Set alphabet and ksize for sketch and search.
     """
-    alphabet_info = config["alphabet_info"]
+    alphabet_defaults = config["alphabet_defaults"]
     input_type = config.get("input_type")
-    if input_type:
-        if input_type in ["protein", "nucleotide"]:
-            if input_type == "protein" and "nucleotide" in alphabet_info.keys():
-                print(f'** input type is protein. Turning off nucleotide searching.')
-                del alphabet_info["nucleotide"]
-                config["alphabet_info"] = alphabet_info
-        else:
-            print(f'** ERROR: input type {input_type} must be either "protein" or "nucleotide"')
-            if strict_mode:
-                print('** exiting.')
-                sys.exit(-1)
-    else:
-        print(f'** ERROR: "input_type: protein" or "input_type: nucleotide" must be specified in the config file')
-        print('** exiting.')
-        sys.exit(-1)
+    alphabet = config.get('alphabet')
+    ksize = config.get('ksize')
+    if not input_type:
+        raise ValueError('"input_type: protein" or "input_type: nucleotide" must be specified in the config file')
+    # nucleotide input
+    elif input_type in ["nucleotide", "dna", "rna"]:
+        if alphabet in ["nucleotide", "dna", "rna"]:
+            config["sketch_type"] = alphabet
+            if not ksize:
+                config["ksize"] = alphabet_defaults["nucleotide"]["ksizes"]
+            config["scaled"] = alphabet_defaults["nucleotide"]["scaled"]
+        # translate to protein
+        elif alphabet in ["protein", "dayhoff", "hp"]:
+            config["sketch_type"] = protein
+            if not ksize:
+                config["ksize"] = alphabet_defaults["nucleotide"]["ksizes"]
+            config["scaled"] = alphabet_defaults["nucleotide"]["scaled"]
+    #protein input
+    elif input_type in ["protein"]:
+        config["sketch_type"] = protein
+        if not ksize:
+            config["ksize"] = alphabet_defaults["nucleotide"]["ksizes"]
+        config["scaled"] = alphabet_defaults["nucleotide"]["scaled"]
+
+    else: #unknown input
+        raise ValueError(f'input type {input_type} must be "protein" or "nucleotide"')
+    return config
 
 
-def check_and_set_alphabets(config, strict_mode=False):
-    """
-    Choose the alphabets for sketching and search
-    """
-    config["strict_mode"] = strict_mode
-    alphaInfo={}
-
-    default_alphabets = config["alphabet_defaults"]
-    alphabets = config.get("alphabets", default_alphabets)
-    # check each alphabet
-    for alpha in alphabets:
-        if alpha not in default_alphabets.keys():
-            print(f'** ERROR: alphabet {alpha} is invalid. Valid alphabets are: {", ".join(default_alphabets)}')
-            if strict_mode:
-                print('** exiting.')
-                sys.exit(-1)
-            else:
-                print(f'Strict mode is off: attempting to continue. Removing {alpha} from alphabet list.')
-                alphabets.remove(alpha)
-        else:
-            # first, set defaults
-            alphaInfo[alpha] = default_alphabets[alpha]
-            # if override values are provided in the config, replace defaults
-            if isinstance(alphabets, dict):
-                alphaInfo[alpha]["ksizes"] = alphabets[alpha].get("ksizes", default_alphabets[alpha]["ksizes"])
-                scaled = alphabets[alpha].get("scaled", default_alphabets[alpha]["scaled"])
-                if not isinstance(scaled, list):
-                    scaled = [scaled]
-                alphaInfo[alpha]["scaled"] = scaled
-    if alphaInfo:
-        config["alphabet_info"] = alphaInfo
-        config = check_input_type(config)
-    else:
-        print(f'** ERROR: no valid alphabets remain')
-        print('** exiting.')
-        sys.exit(-1)
-
-
-def sanitize_path(path):
-    # expand `~`, get absolute path
-    path = os.path.expanduser(path)
-    path = os.path.abspath(path)
-    return path
-
-
-def make_db_fullname(row):
-    row["db_fullname"] = row["db_basename"] + "." + row["alphabet"] + "-k" +  str(row["ksize"]) + "-scaled" + str(row["scaled"])
+def _make_db_fullname(row):
+    row["db_fullname"] = row["db_basename"] + "." + row["alphabet"] + "-k" +  str(row["ksize"])
     return row
 
 
-def load_database_info(databases_file, existing_db_info=None,strict_mode=False):
+def load_databases_csv(databases_file, existing_db_info=None):
     db_file = find_input_file(databases_file)
+    db_info=None
     if '.tsv' in db_file or '.csv' in db_file:
         separator = '\t'
         if '.csv' in db_file:
             separator = ','
         try:
             db_info = pd.read_csv(db_file, sep=separator, index_col=False)
-            assert list(db_info.columns) == ["db_basename","alphabet","ksize","scaled","path","info_path"]
-            db_info = db_info.apply(make_db_fullname, axis=1)
+            assert list(db_info.columns) == ["db_basename","alphabet","ksize","scaled","path","taxonomy_path"]
+            db_info = db_info.apply(_make_db_fullname, axis=1)
             # verify_integrity checks the new index for duplicates.
             db_info.set_index("db_fullname", inplace=True, verify_integrity=True)
             if existing_db_info:
                 db_info = pd.concat(existing_db_info, db_info, verify_integrity=True)
         except Exception as e:
-            sys.stderr.write(f"\n\tError: {db_file} file is not properly formatted. Please fix.\n\n")
             print(e)
+            raise ValueError(f"\n\tError: {db_file} file is not properly formatted. Please fix.\n\n")
     elif '.xls' in db_file:
         try:
             db_info = pd.read_excel(db_file, index_col=False)
             assert list(db_info.columns) == ["db_basename","alphabet","ksize","scaled","path","info_path"]
-            db_info = db_info.apply(make_db_fullname, axis=1)
+            db_info = db_info.apply(_make_db_fullname, axis=1)
             db_info.set_index("db_fullname", inplace=True, verify_integrity=True)
             if existing_db_info:
                 db_info = pd.concat(existing_db_info, db_info, verify_integrity=True)
         except Exception as e:
-            sys.stderr.write(f"\n\tError: {db_file} file is not a properly formatted excel file. Please fix.\n\n")
             print(e)
+            raise ValueError(f"\n\tError: {db_file} file is not a properly formatted excel file. Please fix.\n\n")
     return db_info
 
 
-def check_dbinfo(config, db_basename, dbinfo, alphabet_info, db_input="default", strict_mode=False):
+def find_valid_databases(databases, db_info, config, *, strict=False):
     databases_to_use=[]
-    # get database fullnames
-    available_databases = list(dbinfo.index)
-
-    # construct fullnames that we want to make
-    desired_dbfullnames = []
-    for alpha, alphaInfo in alphabet_info.items():
-        desired_dbfullnames += expand("{name}.{alpha}-k{ksize}-scaled{scaled}", name=db_basename, alpha=alpha, ksize=alphaInfo["ksizes"], scaled=alphaInfo["scaled"])
-    # this now checks alphas, ksizes, scaled!
-    for db in set(desired_dbfullnames):
-        if db in available_databases:
-            databases_to_use.append(db)
+    alpha = config['alphabet']
+    ksize = config['ksize']
+    for db in databases:
+        #check that this alpha/ksize exists in our database info file
+        db_fullname = f"{db}.{alpha}-k{ksize}"
+        if db_fullname in db_info.index:
+            databases_to_use.append(db_fullname)
         else:
-            # get alpha, ksize, scaled from db_fullname
-            alpha_k_scaled = db.split(f"{db_basename}.")[1]
-            this_alpha, this_k, this_scaled = alpha_k_scaled.split("-")
-            print(f'** ERROR: A {db_input} {db_basename} database is not provided for {this_alpha}, {this_k}, {this_scaled}.')
-            if strict_mode:
-                print('** Strict mode is on. Exiting.')
-                sys.exit(-1)
+            if strict:
+                raise ValueError(f'The {db} database is not provided for alphabet:{alpha}, ksize:{ksize}')
             else:
                 print(f'Strict mode is off: attempting to continue. Removing {db} from search databases list.')
+
+    if not databases_to_use:
+        raise ValueError('No valid databases provided.')
+
     return databases_to_use
 
-def check_databases(config):
-    strict_mode = config.get("strict_mode", False)
-    databases=[]
-    # first, get load all the database info
-    default_dbinfo = config["default_database_info"]
-    default_db_file = srcdir(default_dbinfo)
-    # load databases csv
-    db_info = load_database_info(default_db_file)
+
+def check_and_set_databases(config, *, strict=False):
+    # first, get load all the database info from default and user CSVs
+    default_db_file = srcdir(config["default_database_info"])
+    db_info = load_databases_csv(default_db_file)
     # integrate user database info
     if config.get("user_database_info"):
        # load user database info
-       db_info = load_database_info(config["user_database_info"], existing_db_info=db_info)
-
-    # store db_info in config
+       db_info = load_databases_csv(config["user_database_info"], existing_db_info=db_info)
+    # store database info in config to enable easy download and use
     config["database_info"] = db_info
 
-    # now figure out what database(s) to use!
-    # first, default databases
-    default_databases = config.get("default_databases", [])
-    no_use_defaults = config.get("turn_off_default_databases", False)
-
-    alphabet_info = config["alphabet_info"]
-    if not no_use_defaults:
-        # check that we have the info for these default databases
-        for db in default_databases:
-            db_names  = check_dbinfo(config, db, db_info, alphabet_info, strict_mode=strict_mode)
-            databases+=db_names
-
-    ## add user databases if provided and relevant info is available
-    user_dbs = config.get("search_databases", [])
-
-    for db in user_dbs:
-        # add database to list of databases to search
-        db_names = check_dbinfo(config, db, db_info, alphabet_info, db_input="user", strict_mode=strict_mode)
-        databases+=db_names
-
-    if not databases:
+    #now check if specified search databases are valid
+    databases = config.get('search_databases', [])
+    if not isinstance(databases, list):
+        databases = [databases]
+    try:
+        valid_databases = find_valid_databases(databases, db_info, config)
+    except ValueError as exc:
         avail_dbs = list(db_info.index)
-        print('\n** ERROR: no valid databases selected. ')
-        print('Please choose from the available databases '\
-              'or do not disable default databases. \n' )
         print('Available databases are: ',*avail_dbs, sep="\n" )
-        print('** exiting.')
+        raise
+    config['valid_databases'] = valid_databases
+    return config
+
+
+def process_user_config(config):
+    strict_mode = config.get('strict_mode', False)
+    try:
+        samples = read_samples(config, strict=strict_mode)
+        config = check_and_set_alpha_ksize(config, strict=strict_mode)
+        config = check_and_set_databases(config, strict=strict_mode)
+    except ValueError as exc:
+        print(f'ERROR: Could not properly integrate configuration values.')
+        print(f'{str(exc)}')
         sys.exit(-1)
 
-    # store database names in config
-    config["databases"] = databases
-    return databases
-
-def build_index_names(config):
-    strict_mode = config.get("strict_mode", False)
-    index_names = []
-    basename = config["basename"]
-    alphabet_info = config["alphabet_info"]
-    for alpha, alphaInfo in alphabet_info.items():
-        index_names+=expand("{basename}.{alpha}-k{ksize}-scaled{scaled}", basename=basename, alpha=alpha, ksize=alphaInfo["ksizes"], scaled=alphaInfo["scaled"])
-    return index_names
-
+    return config
 
